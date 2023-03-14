e2380a55a8428bd35dbf6194868edc5ea4deecb8
isaac-candelario-escobar-lopez <isacel97@gmail.com>
2023-03-13 18:17:06 -0600

    avance tabla usuarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->unsignedBigInteger('role_id');
+            $table->string('telefono');
             $table->foreign('role_id')->references('id')->on('roles');
         });
     }
