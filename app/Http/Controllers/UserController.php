27fdcd2ee2f079aa04a38211bb8063671d0b8af0
isaac-candelario-escobar-lopez <isacel97@gmail.com>
2023-03-15 20:58:28 -0600

    login no sirve

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,21 +7,110 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function createUser(Request $request)
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
     {
         $data = $request->all();
-        $user = new User();
-        $user->name = $data['name'];
-        $user->email = $data['email'];
-        $user->password = md5($data['password']);
-        $user->role_id = $data['role_id'];
-        $user->telefono = $data['telefono'];
-        $user->save();
-        if ($user->save()) {
-            return response($user, 200);
-        } else {
-            return response('Error al crear usuario', 500);
-        }
         
+        $validar = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+            'role_id' => 'required|numeric',
+            'telefono' => 'required|numeric|digits:10',
+        ]);
+        
+
+        return response()->json([
+            $data,
+        ], 201);
+        // $user = new User();
+        // $user->name = $data['name'];
+        // $user->email = $data['email'];
+        // $user->password = $data['password'];
+        // $user->role_id = $data['role_id'];
+        // $user->telefono = $data['telefono'];
+        // $user->save();
+
+        // if ($user->save()) {
+        //     return response()->json([
+        //         'message' => 'Usuario creado correctamente',
+        //     ], 201);
+        // } else {
+        //     return response()->json([
+        //         'message' => 'Error al crear el usuario',
+        //     ], 500);
+        // }
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
