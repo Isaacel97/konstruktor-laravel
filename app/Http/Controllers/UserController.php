5bafdc696a3c9eeb0bf564f84a9aed65a2464129
Alex Santiago <sg.alex32@gmail.com>
2023-03-22 00:37:26 -0600

    login y registro

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 class UserController extends Controller
 {
     /**
@@ -19,52 +21,52 @@ class UserController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+        *
+        * @return \Illuminate\Http\Response
      */
     public function create(Request $request)
     {
         $data = $request->all();
-
-        $validated = $request->validate([
-            'name' => 'required',
-            'apellido' => 'required',
+       
+        $validated = Validator::make($request->all(),[
+            'name' => 'required', 
+            'apellidos' => 'required',
             'email' => 'required|email',
-            'password' => 'required|min:8',
+            'password' => 'required|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/',
             'role_id' => 'required|numeric',
-            'telefono' => 'required|numeric|digits:10',
-        ]);
-        
-        try {
-            $user = new User();
-            $user->name = $data['name'];
-            $user->apellidos = $data['apellido'];
-            $user->email = $data['email'];
-            $user->password = md5($data['password']);
-            $user->role_id = $data['role_id'];
-            $user->telefono = $data['telefono'];
-            $user->save();
+            'telefono' => 'required|numeric|digits:10'
 
+        ]);
+
+        if ($validated->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'validation error',
+                'errors' => $validated->errors()
+            ], 401);
+        } 
+    
+        $user = new User();
+        $user->name = $data['name'];
+        $user->apellidos = $data['apellidos'];
+        $user->email = $data['email'];
+        $user->password = Hash::make( $data['password']);
+        
+        $user->role_id = $data['role_id'];
+        $user->telefono = $data['telefono'];
+        try {
+            $user->save();
             return response()->json([
                 'message' => 'Usuario creado correctamente',
-            ], 201);
-        } catch (\Throwable $th) {
+                'status' => 200
+            ]);
+        } catch (\Throwable $th) {    
             return response()->json([
-                'message' => 'Error al crear el usuario',
-                'error' => $th->getMessage()
-            ], 500);
+                'message' => 'Este usuario ya existe',
+                'error' => $th->getMessage(),
+                'status' => 500
+            ]);
         }
-
-        // if ($user->save()) {
-        //     return response()->json([
-        //         'message' => 'Usuario creado correctamente',
-        //     ], 201);
-        // } else {
-        //     return response()->json([
-        //         'message' => 'Error al crear el usuario',
-        //     ], 500);
-        // }
-        
     }
 
     /**
@@ -121,5 +123,44 @@ class UserController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function login (Request $request) { 
+        //login auth
+        $data = $request->all();
+        $validated = Validator::make($request->all(),[
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+        
+        if ($validated->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'validation error',
+                'errors' => $validated->errors()
+            ], 401);
+        }
+        $user = User::where('email', $data['email'])->first();
+
+        if ($user) {
+            if (Hash::check($data['password'], $user->password)) {
+                return response()->json([
+                    'message' => 'Usuario logueado correctamente',
+                    'user' => $user,
+                    'status' => 200,
+                    'token' => hash('sha256', $plainTextToken = Str::random(40)),
+                ]);
+            } else {
+                return response()->json([
+                    'message' => 'Contraseña incorrecta',
+                    'status' => 401
+                ], );
+            }
+        } else {
+            return response()->json([
+                'message' => 'Usuario no encontrado',
+                'status' => 404 
+            ], );
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -67,3 +67,5 @@ Route::post('user/create/', function(Request $request) {
     $resp = $controller->create($request);
     return $resp;
 });
+
+Route::post('user/login/', [ UserController::class, 'login' ])->name('login');
